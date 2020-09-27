9757ceddbc91aa558edb1a0384f76c1262191e20
Pierangelo Martellone <64583687+pmartellone13@users.noreply.github.com>
2020-09-27 19:09:39 +0200

    Deploy into master

--- Client/ProgettoGUI/GeneratedFiles/ui_textedit.h
+++ Client/ProgettoGUI/GeneratedFiles/ui_textedit.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'textedit.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.3
+** Created by: Qt User Interface Compiler version 5.15.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -32,7 +32,7 @@ public:
 
     void retranslateUi(QWidget *textedit)
     {
-        textedit->setWindowTitle(QApplication::translate("textedit", "textedit", nullptr));
+        textedit->setWindowTitle(QCoreApplication::translate("textedit", "textedit", nullptr));
     } // retranslateUi
 
 };
